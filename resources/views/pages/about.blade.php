1785fb728bd648767f17902b798a5c0c15fc6d39
Jonathan Mead <lylyahiko@gmail.com>
2017-04-13 13:15:37 -0400

    Updating about page and some slight style modifications

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -4,10 +4,72 @@
     <div class="flex-center position-ref">
         <div class="content">
             <div class="title m-b-md">
-                About Me
+                About
             </div>
+            <section><a href="http://placehold.it"><img src="http://placehold.it/500x500"></a></section>
             <section class="page">
-            <div><p>Insert fun stuff to tell people who I am.</p></div>
+            <h4>Work Experience</h4>
+            <div class="row">
+                <div class="col-2">
+                    <div class="d-flex w-100">
+                      <p class="align-left">Alter Imaging</p>
+                    </div>
+                    <p class="align-left">Title: <small><strong>Web Developer</strong></small></p>
+                    <p class="align-left">Started: <small><strong>Feb 2017</strong></small></p>
+                    <p class="align-left">Left: <small><strong>Still working there</strong></small></p>
+                    <p class="align-left">Responsibilities: 
+                        <small><strong>Responsible for creation and maintence of new and current projects.
+                        Worked primarily in PHP using Laravel frameworks</strong></small></p>
+                    <p class="align-left">Projects of Note: <small><strong><a href="www.tradetreeonline.com">Trade Tree Online</a>
+                        <br><a href="isleblue.co">Isle Blue</a></strong></small></p>
+                </div>
+                <div class="col-2">
+                    <div class="d-flex w-100">
+                      <p class="align-left">Beanstalk Data</p>
+                    </div>
+                    <p class="align-left">Title: <small><strong>Web Developer</strong></small></p>
+                    <p class="align-left">Started: <small><strong>Febuary 2016</strong></small></p>
+                    <p class="align-left">Left: <small><strong>November 2016</strong></small></p>
+                    <p class="align-left">Responsibilities: 
+                    <small><strong>Responsible for both front and backend development of new projects.
+                        Primarily worked in Javascript with a small amount of PHP</strong></small></p>
+                    <p class="align-left">Projects of Note: <small><strong><a href="www.pollo-tropical.com">Pollo Tropical</a>
+                    <br><a href="www.tacocabana.com">Taco Cabana</a></strong></small>
+                    <br><small><strong>Developed a project to integrate a third party service with an internal system. This was done using a php script that broke down data and transmitted it via apis.</strong></small></p></p>
+                </div>
+                 <div class="col-2">
+                    <div class="d-flex w-100">
+                      <p class="align-left">Better Car People</p>
+                    </div>
+                    <p class="align-left">Title: <small><strong>Web Developer</strong></small></p>
+                    <p class="align-left">Started: <small><strong>October 2014</strong></small></p>
+                    <p class="align-left">Left: <small><strong>Febuary 2016</strong></small></p>
+                    <p class="align-left">Responsibilities: 
+                    <small><strong>Responsible for maintaining core product.
+                    Primarily in PHP utilizing an MVC framework</strong></small></p>
+                    <p class="align-left">Projects of Note: <small><strong><a href="overnightbdc.com">OvernightBDC</a>
+                    <small></small></p>
+                </div>
+            </div>
+            </section>
+            <section class="page">
+            <h4>Product Experience</h4>
+            <div class="row">
+                <div class="col-6">
+                    <p>Programming Languages: <small><strong>PHP (primary), Javascript (primary), C#/C++ (Mostly used in school), Visual Basic (Moslty used in school), Java (exposure)</strong></small></p>
+                    <p>Databases: <small><strong>Mostly MySQL, some SQL</strong></small></p>
+                    <p>Operating Systems: <small><strong>Most of my coding has been on a Mac but I'm very familiar with navigating and operating a PC.</strong></small></p>
+                </div>
+            </div>
+            </section>
+            <section class="page">
+            <h4>Hobbies</h4>
+            <div class="row">
+                <div class="col-6">
+                    <p>Programming: <small><strong>I've been programming since I was 8 years old working with Qbasic. I program in my free time on and off mostly working on small projects to entertain myself.</strong></small></p>
+                    <p>Video Games: <small><strong>Being 29 I fit in the perfect age group for this to be a primary hobby of mine albeit not very constructive. I enjoyed video games and programming so much that I earned an associates degree in Simulation and Game Design. My primary focus was artificial intelligence.</strong></small></p>
+                </div>
+            </div>
             </section>
         </div>
     </div>
